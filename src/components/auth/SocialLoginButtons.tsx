f62389723102f7b141ed1cf5ed1c2284ac28ec95
Sole Rios <soledadrios38@gmail.com>
2026-09-21 14:24:06 -0300

    Ocultar temporalmente el acceso con Facebook

--- src/components/auth/SocialLoginButtons.tsx
+++ src/components/auth/SocialLoginButtons.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 type Provider = "google" | "facebook";
 
-const SHOW_FACEBOOK_LOGIN = true;
+const SHOW_FACEBOOK_LOGIN =false;
 
 export default function SocialLoginButtons({
   redirectTo = "/",
